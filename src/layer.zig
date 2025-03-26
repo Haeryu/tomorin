d51e03b7477692012b9a27c4a111fdd6df423f8f
Haeryu <hae06177@gmail.com>
2025-03-27 02:41:53 +0900

    made CausalSelfAttention

--- src/layer.zig
+++ src/layer.zig
@@ -20,6 +20,11 @@ const layerNormEx = function.layerNormEx;
 const averagePoolingEx = function.averagePoolingEx;
 const addEx = function.addEx;
 const embedEx = function.embedEx;
+const splitEx = function.splitEx;
+const transposeExEx = function.transposeExEx;
+const scaleEx = function.scaleEx;
+const maskedFillEx = function.maskedFillEx;
+const softmaxEx = function.softmaxEx;
 
 const dbg = @import("util.zig").debugPrintGpuTensor;
 
@@ -301,6 +306,7 @@ pub fn Linear(comptime T: type) type {
         winit: WInit,
 
         pub const WInit = enum {
+            normal,
             xavier,
             he_normal,
             he_uniform,
@@ -365,6 +371,7 @@ pub fn Linear(comptime T: type) type {
             // try w_tensor.scale(1.0 / @as(T, @floatFromInt(self.in_size.?)), self.context.stream);
 
             switch (self.winit) {
+                .normal => try w_tensor.fillNormalDistribution(0.0, 1.0, self.context.cuda_context, self.context.stream),
                 .xavier => try w_tensor.fillXavierUniform(self.context.cuda_context, self.context.stream),
                 .he_normal => try w_tensor.fillHeNormal(self.context.cuda_context, self.context.stream),
                 .he_uniform => try w_tensor.fillHeUniform(self.context.cuda_context, self.context.stream),
@@ -1645,19 +1652,19 @@ pub fn Embedding(comptime T: type) type {
             chain: *Chain,
         ) !Self {
             // Create the weight tensor [num_embeddings, embedding_dim]
-            var weight_tensor: GPUTensor(T) = try .initAsync(&.{ num_embeddings, embedding_dim }, context.stream);
-            errdefer weight_tensor.deinitAsync(context.stream);
+            var w_tensor: GPUTensor(T) = try .initAsync(&.{ num_embeddings, embedding_dim }, context.stream);
+            errdefer w_tensor.deinitAsync(context.stream);
 
             // Initialize the weight tensor based on winit
             switch (winit) {
-                .normal => try weight_tensor.fillNormalDistribution(0.0, 1.0, context.cuda_context, context.stream),
-                .xavier => try weight_tensor.fillXavierUniform(context.cuda_context, context.stream),
-                .he_normal => try weight_tensor.fillHeNormal(context.cuda_context, context.stream),
-                .he_uniform => try weight_tensor.fillHeUniform(context.cuda_context, context.stream),
+                .normal => try w_tensor.fillNormalDistribution(0.0, 1.0, context.cuda_context, context.stream),
+                .xavier => try w_tensor.fillXavierUniform(context.cuda_context, context.stream),
+                .he_normal => try w_tensor.fillHeNormal(context.cuda_context, context.stream),
+                .he_uniform => try w_tensor.fillHeUniform(context.cuda_context, context.stream),
             }
 
             // Register the weight as a learnable variable
-            const w = try chain.createVariable(T, weight_tensor.move(), "w");
+            const w = try chain.createVariable(T, w_tensor.move(), "w");
 
             return .{
                 .fields = .{
@@ -1673,6 +1680,137 @@ pub fn Embedding(comptime T: type) type {
 
         pub fn forward(self: *Self, indices: *TaggedVar, chain: *Chain) !*TaggedVar {
             return try embedEx(T, self.fields.w.?, indices, chain);
+        }
+    };
+}
+
+pub fn CausalSelfAttention(comptime T: type) type {
+    return struct {
+        pub usingnamespace LayerDecorator(Self);
+
+        fields: LayerFieldsFactory(
+            &.{},
+            &.{
+                .{ "c_attn", Linear(T) },
+                .{ "c_proj", Linear(T) },
+                .{ "attn_dropout", Dropout(T) },
+                .{ "resid_dropout", Dropout(T) },
+            },
+        ),
+        n_head: usize,
+        n_embd: usize,
+        // dropout_ratio: T,
+        bias: GPUTensor(T),
+        context: *Context,
+
+        const Config = struct {
+            n_embd: usize,
+            n_head: usize,
+            block_size: usize,
+            nobias: bool,
+            dropout_ratio: T,
+        };
+
+        const Self = @This();
+
+        pub fn init(
+            config: *const Config,
+            context: *Context,
+            chain: *Chain,
+        ) !Self {
+            std.debug.assert(config.n_embd % config.n_head == 0);
+            var c_attn: Linear(T) = try .init(config.n_embd, 3 * config.n_embd, config.nobias, .xavier, context, chain);
+            errdefer c_attn.destroy();
+            var c_proj: Linear(T) = try .init(config.n_embd, config.n_embd, config.nobias, .xavier, context, chain);
+            errdefer c_proj.destroy();
+            const attn_dropout: Dropout(T) = .init(config.dropout_ratio);
+            const resid_dropout: Dropout(T) = .init(config.dropout_ratio);
+
+            var bias: GPUTensor(T) = try .initAsync(.{ config.block_size, config.block_size }, context.stream);
+            errdefer bias.deinitAsync(context.stream);
+            try bias.fill(1.0, context.stream);
+            try bias.tril(0.0, context.stream);
+            try bias.reshape(.{ 1, 1, config.block_size, config.block_size });
+
+            return .{
+                .fields = .{
+                    .c_attn = c_attn,
+                    .c_proj = c_proj,
+                    .attn_dropout = attn_dropout,
+                    .resid_dropout = resid_dropout,
+                },
+                .n_head = config.n_head,
+                .n_embd = config.n_embd,
+                .bias = bias,
+                .context = context,
+            };
+        }
+
+        pub fn predestroy(self: *Self) void {
+            self.bias.deinitAsync(self.context.stream);
+        }
+
+        pub fn forward(self: *Self, x: *TaggedVar, train: bool, chain: *Chain) !*TaggedVar {
+            const x_shape = x.getShape();
+            const b = x_shape[0];
+            const t = x_shape[1];
+            const c = x_shape[2];
+
+            if (t > self.bias.getShape()[2]) return error.SequenceLengthExceedsBlockSize;
+
+            const attn = try self.fields.c_attn.forward(x, chain);
+            var q, var k, var v = try splitEx(3, T, attn, 2, chain);
+            q = try reshapeEx(T, q, &.{ b, t, self.n_head, c / self.n_head }, chain);
+            k = try reshapeEx(T, k, &.{ b, t, self.n_head, c / self.n_head }, chain);
+            v = try reshapeEx(T, v, &.{ b, t, self.n_head, c / self.n_head }, chain);
+            q = try transposeExEx(T, q, &.{ 0, 2, 1, 3 }, chain); // (B, nh, T, hs)
+            k = try transposeExEx(T, k, &.{ 0, 2, 1, 3 }, chain); // (B, nh, T, hs)
+            v = try transposeExEx(T, v, &.{ 0, 2, 1, 3 }, chain); // (B, nh, T, hs)
+
+            const k_transpose = try transposeExEx(T, k, &.{ 0, 1, 3, 2 }, chain);
+            var att = try matmulEx(T, q, k_transpose, chain);
+            att = try scaleEx(T, att, 1.0 / @sqrt(@as(T, @floatFromInt(k_transpose.getShape()[k_transpose.getShape().len - 1]))));
+
+            var mask = try self.bias.getItem(
+                self.context.allocator,
+                &.{
+                    .all,
+                    .all,
+                    .{
+                        .start = 0,
+                        .stop = t,
+                        .step = 1,
+                    },
+                    .{
+                        .start = 0,
+                        .stop = t,
+                        .step = 1,
+                    },
+                },
+                self.context.stream,
+            );
+            defer mask.deinitAsync(self.context.stream);
+            var mask_broad = try mask.broadcastTo(att.getShape(), self.context.stream);
+            defer mask_broad.deinitAsync(self.context.stream);
+
+            att = try maskedFillEx(
+                T,
+                att,
+                .{
+                    .mask = mask_broad.move(),
+                    .val = -std.math.inf(T),
+                },
+                chain,
+            );
+            att = try softmaxEx(T, att, .{3}, chain);
+            att = try self.fields.attn_dropout.forward(att, train, chain);
+            var y = try matmulEx(T, att, v, chain);
+            y = try transposeExEx(T, y, &.{ 0, 2, 1, 3 }, chain);
+            y = try reshapeEx(T, y, &.{ b, t, c }, chain);
+            y = try self.fields.c_proj.forward(y, chain);
+            y = try self.fields.resid_dropout.forward(y, train, chain);
+
+            return y;
         }
     };
 }
